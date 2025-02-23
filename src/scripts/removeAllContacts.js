467c66d558721fd070aca9932eacab9b6a81b495
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-02-23 10:41:01 +0100

    finish

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,10 +1,10 @@
-import { PATH_DB } from '../constants/contacts.js';
+
 import { writeContacts } from '../utils/writeContacts.js';
 
 
 export const removeAllContacts = async () => {
     try {
-        await writeContacts(PATH_DB, '[]', 'utf-8'); // Очищаем файл
+        await writeContacts([]); // Очищаем файл
         console.log('Все контакты удалены.');
     } catch (error) {
         console.error('Ошибка при удалении контактов:', error);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import { PATH_DB } from '../constants/contacts.js';
-import * as fs from 'fs/promises';
+import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
     await fs.writeFile(path.resolve(PATH_DB), JSON.stringify(updatedContacts, null, 2 ));
